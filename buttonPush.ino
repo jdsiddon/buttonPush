763462f0e7888a911b12aed1523a9a6fd34799a7
jsdiddon <justin@delaris.com>
2013-09-09 22:44:29 -0700

    Added logic to accept one button push for 'on' state and second button push for 'off' state

--- buttonPush.ino
+++ buttonPush.ino
@@ -1,5 +1,12 @@
 int onOffState = LOW; // Variable to hold current button state
 int numberButtonPushes = 0;
+int lastButtonState = LOW; // The previous reading from the input pin
+int buttonState;
+
+// Debounce variables
+long lastDebounceTime = 0;
+long debounceDelay = 50; 
+
 
 void setup () {
   Serial.begin(9600);
@@ -7,31 +14,42 @@ void setup () {
 }
 
 void loop () {
-  int buttonValue = digitalRead(2);    // Set buttonValue to digital output of pin 2
+  int reading = digitalRead(2);    // Set buttonValue to digital output of pin 2
   
-  // If button is pushed    
-  if (buttonValue == HIGH) {   
-    numberButtonPushes++;  
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        numberButtonPushes++;  
     
-    // If numberButtonPushes divided by 2 has a remainder not equal to 0 and onOffState is equal to LOW.
-    if(numberButtonPushes % 2 != 0 && onOffState == LOW) {     
-      Serial.println(" On");
-      onOffState = HIGH;
-    }
+        // If numberButtonPushes divided by 2 has a remainder not equal to 0 and onOffState is equal to LOW.
+        if(numberButtonPushes % 2 != 0 && onOffState == LOW) {     
+          Serial.println(" On");
+          onOffState = HIGH;
+        }
     
-    // If the number of button pushes divided by two has a remainder equal to 1 and noOffState is equal to LOW. 
-    if (numberButtonPushes % 2 == 0 && onOffState == HIGH) {    
-      Serial.println(" Off");
-      onOffState = LOW;  
+        // If the number of button pushes divided by two has a remainder equal to 1 and noOffState is equal to LOW. 
+        if (numberButtonPushes % 2 == 0 && onOffState == HIGH) {    
+          Serial.println(" Off");
+          onOffState = LOW;  
+        }
+      }
     }
-    delay(30);
   }
   
-  // If button isn't pushed
-  if (buttonValue == LOW) {
-    //numberButtonPushes++; commented out because as long as button is low will keep incrementing
-  }
-  
-  //onOffState = buttonValue;
-  //wait(
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading; 
 }
